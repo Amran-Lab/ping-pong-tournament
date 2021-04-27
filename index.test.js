264e3f808ce9cbbaf75d26807ff3abac081e86c6
Amran Uddin <amran.uddin@sykescottages.co.uk>
2021-04-27 12:53:41 +0100

    first commit

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const { numToRoman } = require('./index')
+
+
+describe('Test converting Arabic numbers to Roman numerals', () => {
+    describe('Converts numbers 1-3', () => {
+        it('Smoke', () => {
+            expect(I).toEqual('I');
+        });
+
+})
+})
