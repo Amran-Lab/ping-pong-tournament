fc52274b53fe82ae1ab668a6455989b3c4be5c19
AstridCaballero <astridcaballero@gmail.com>
2021-04-27 14:25:48 +0100

    multiplity by 2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function sortArrAsc(arr) {
     return arr.sort()
   }
+
+  function multiplyArray(arr) {
+    return arr * 2
+  }
   module.exports = sortArrAsc;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
-const sortArrAsc = require('./index');
+const { sortArrAsc, multiplyArray } = require('./index');
+// const multiplyArray = require ('./index');
 test('adds 1 + 2 to equal 3', () => {
     expect(2+2).toEqual(4);
     expect(true).toBeTruthy();
@@ -14,4 +15,10 @@ describe('Sorting Array', () => {
     it('sort number array', () => {
         expect(sortArrAsc([1,5,7,4])).toEqual([1,4,5,7]);
     });
+});
+
+describe('operations on array', () => {
+    it('multiply array', () => {
+        expect(multiplyArray([1,2,3,4])).toEqual([2,4,9,16]);
+    });   
 });
